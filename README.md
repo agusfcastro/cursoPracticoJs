ae56b44b1c3cfca8f0934f5d696caf48675f74a9
agusfcastro <c.federico.agustin@gmail.com>
2022-03-06 21:59:49 -0300

    Calculando figuras geometricas con js

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # cursoPracticoJs
+
+...
+
+## Taller #1: Figuras geometricas
+
+- PRIMERO: definir las formulas
+- SEGUNDO: implementar figuras en Js
+- TERCERO: crear funciones
+- CUARTO: integrar Js con HTML
--- figuras.js
+++ figuras.js
@@ -1,1 +1,54 @@
-console.log('hello world')
+//codigo del cuadrado
+console.group('Cuadrado')
+const ladoCuadrado = 5;
+console.log('los lados del cuadrado miden: ' + ladoCuadrado + 'cm');
+
+const perimetroCuadrado = ladoCuadrado * 4
+console.log('El perimetro del cuadrado es de: ' + perimetroCuadrado + 'cm');
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado
+console.log('El area del cuadrado es de: ' + areaCuadrado + 'cm2');
+console.groupEnd();
+
+//codigo del triangulo
+console.group('Tirangulo');
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log(
+    'los lados del triangulo miden: ' 
+    + ladoTriangulo1 
+    + 'cms, mientras que la base mide: '
+    + baseTriangulo
+     );
+
+const alturaTriangulo = 5.5;
+console.log('La altura del triangulo es de:' + alturaTriangulo + 'cms');
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2
+console.log('El area del triangulo es de: ' + areaTriangulo + 'cm2')
+
+console.groupEnd();
+
+//codigo ciruclo
+console.group('Ciruclo');
+
+const radioCirculo = 4
+console.log('el radio del ciruclo es de: ' + radioCirculo + 'cms')
+
+const diametroCirculo = radioCirculo * 2
+console.log('diametro del dicurlo es de: ' + diametroCirculo + 'cms')
+
+const pi = Math.PI
+
+const circunsferenciaCiruclo = diametroCirculo * pi
+console.log('la circunsferencia del ciruclo es de: ' + circunsferenciaCiruclo + 'cm');
+
+const areaCiruclo = (radioCirculo * radioCirculo) * pi
+console.log('el area del circulo es de: ' + areaCiruclo + 'cm2')
+
+console.groupEnd();
+
+
+
