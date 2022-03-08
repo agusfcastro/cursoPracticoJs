031c7cf7ffdad547622aa3d2c5caee489ceb373a
agusfcastro <c.federico.agustin@gmail.com>
2022-03-08 10:20:00 -0300

    Clase 9: agregue cálculo de altura para triangulos isosceles

--- figuras.js
+++ figuras.js
@@ -1,5 +1,4 @@
 
-
 //codigo del cuadrado
 function perimetroCuadrado(lado) {
     return lado * 4;
@@ -10,7 +9,44 @@ function areaCuadrado(lado) {
 }
 
 
-    // Interacción con HTML
+//codigo del triangulo
+function perimetroTriangulo(lado, lado1, base) {
+    return lado + lado1 + base;
+}
+
+function areaTriangulo(base, altura) {
+    return base * altura;
+}
+
+    // triangulo isoseles
+function alturaIsosceles(lado, lado2, base) {
+    if(lado == lado2 && lado != base){
+        return Math.sqrt((lado**2) - ((base**2) / 4))
+    } 
+    else {
+        return 'No es un triangulo isosceles'
+    }
+}
+
+
+//codigo ciruclo
+const PI = Math.PI
+
+function diametroCirculo(radio) {
+    return radio * 2;
+}
+
+function perimetroCiruclo(radio) {
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+}
+
+function areaCirculo(radio) {
+    return (radio * radio) * PI;
+}
+
+
+//Interacción con HTML
 function calcularPerimetroCuadrado() {
     const input = document.getElementById("inputCuadrado");
     const lado = input.value;
@@ -28,17 +64,6 @@ function calcularAreaCuadrado() {
 }
 
 
-//codigo del triangulo
-function perimetroTriangulo(lado, lado1, base) {
-    return lado + lado1 + base;
-}
-
-function areaTriangulo(base, altura) {
-    return base * altura;
-}
-
-
-    // interacción con HTML
 function calcularPerimetroTriangulo() {
     const input = document.getElementById("inputTriangulo");
     const lado = Number(input.value);
@@ -64,24 +89,21 @@ function calcularAreaTriangulo() {
     alert(area);
 }
 
-//codigo ciruclo
-const PI = Math.PI
+function calcularAlturaIsosceles() {
+    const input = document.getElementById("inputTriangulo");
+    const value = input.value;
 
-function diametroCirculo(radio) {
-    return radio * 2;
+    const input1 = document.getElementById("inputTriangulo1");
+    const value1 = input1.value;
+
+    const input2 = document.getElementById("inputTriangulo2")
+    const value2 = input2.value;
+
+    const altura = alturaIsosceles(value, value1, value2);
+    alert(altura);
 }
 
-function perimetroCiruclo(radio) {
-    const diametro = diametroCirculo(radio);
-    return diametro * PI;
-}
 
-function areaCirculo(radio) {
-    return (radio * radio) * PI;
-}
-
-
-    //Intearcción con HTML
 function calcularDiametroCirculo() {
     const input = document.getElementById("inputCirculo");
     const value = input.value;
